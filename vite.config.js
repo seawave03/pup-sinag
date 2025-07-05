b8c94bef414093a503a8b7f3d1815e183c5c126e
migzsh <funtilonh023@gmail.com>
2025-07-05 14:37:52 +0800

    refactor(vite.config.js): streamline import statements and format configuration for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()],
-    base: '/pup-sinag/',
-})
+  plugins: [tailwindcss(), react()],
+  base: '/pup-sinag',
+});
